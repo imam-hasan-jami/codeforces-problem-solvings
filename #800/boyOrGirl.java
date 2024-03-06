0e7bc3bb131c3fec8f21027cd1d6474c3c55e5bd
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-03-07 02:39:56 +0600

    Add files via upload

---
+++ #800/boyOrGirl.java
@@ -0,0 +1,12 @@
+package problemsolving;
+
+import java.util.*;
+
+public class boyOrGirl {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        int count = (int) word.chars().distinct().count();
+        System.out.println(count % 2 == 0 ? "CHAT WITH HER!" : "IGNORE HIM!");
+    }
+}
