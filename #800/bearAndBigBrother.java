062f06f19cc0faaff38f291c50e968c593304f33
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-03-08 02:01:46 +0600

    Add files via upload

---
+++ #800/bearAndBigBrother.java
@@ -0,0 +1,23 @@
+package problemsolving;
+import java.util.*;
+
+public class bearAndBigBrother {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int count =0;
+
+        while(a <= b) {
+            count++;
+
+            a = a * 3;
+            b = b * 2;
+        }
+
+        System.out.println(count);
+
+        sc.close();
+    }    
+}
