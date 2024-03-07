88ebf2b49addd339291533c051c14534d9ed3fdb
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-03-08 02:31:37 +0600

    Add files via upload

---
+++ #800/elephant.java
@@ -0,0 +1,13 @@
+package problemsolving;
+import java.util.*;
+
+public class elephant{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println((n+4)/5);
+
+        sc.close();
+    }
+}
