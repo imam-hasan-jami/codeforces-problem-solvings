c2010042166d864d2a8a78655b38704deebea42e
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-03-09 00:43:21 +0600

    Add files via upload

---
+++ #800/soldierAndBananas.java
@@ -0,0 +1,24 @@
+package problemsolving;
+
+import java.util.*;
+
+public class soldierAndBananas {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int n = sc.nextInt();
+        int w = sc.nextInt();
+
+        int totalCost = 0;
+
+        for(int i=1; i<=w; i++) {
+            totalCost += i*k;
+        }
+
+        if((totalCost - n) < 0) {
+            System.out.println(0);
+        } else {
+            System.out.println(totalCost - n);
+        }
+    }
+}
