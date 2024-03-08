f444ea8b745ab52b1f9c87182e3faa3df132b9bd
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-03-08 22:40:02 +0600

    Add files via upload

---
+++ #800/word.java
@@ -0,0 +1,27 @@
+package problemsolving;
+
+import java.util.*;
+
+public class word {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+
+        int uppercase = 0;
+        int lowercase = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) >= 65 && word.charAt(i) <= 90) {
+                uppercase++;
+            } else {
+                lowercase++;
+            }
+        }
+
+        if(lowercase > uppercase || lowercase == uppercase) {
+            System.out.println(word.toLowerCase());
+        } else {
+            System.out.println(word.toUpperCase());
+        }
+    }
+}
