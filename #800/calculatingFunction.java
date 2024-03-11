102394ba6d79a7fc94a906279f977ce529716f76
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-03-12 03:15:07 +0600

    Add files via upload

---
+++ #800/calculatingFunction.java
@@ -0,0 +1,17 @@
+package problemsolving;
+
+import java.util.*;
+
+public class calculatingFunction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+
+        if(n % 2 == 0) {
+            System.out.println(n / 2);
+        } else {
+            System.out.println(-(n/2)-1);
+        }
+        sc.close();
+    }
+}
