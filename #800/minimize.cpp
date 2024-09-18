142f306898ed85ecde951f1c7b076cc9707d8ac5
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-09-18 18:36:27 +0600

    2009A - Minimize!

---
+++ #800/minimize.cpp
@@ -0,0 +1,15 @@
+//2009A - Minimize!
+#include <iostream>
+using namespace std;
+
+int main() {
+    int testCase;
+    cin >> testCase;
+    while(testCase--) {
+        int a,b;
+        cin >> a >> b;
+        cout << b-a << endl;
+    }
+
+    return 0;
+}
