0f2a71bb41a1461c36a80821a2aca22b442f3f01
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-09-24 11:56:00 +0600

    1950A - Stair, Peak, or Neither?

---
+++ #800/stairPeakorNeither.cpp
@@ -0,0 +1,24 @@
+//1950A - Stair, Peak, or Neither?
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int testCases;
+    cin >> testCases;
+    while(testCases--) {
+        int a,b,c;
+        cin >> a >> b >> c;
+        if(a < b && b < c) {
+            cout << "STAIR" << endl;
+        }
+        else if(a < b && b > c) {
+            cout << "PEAK" << endl;
+        }
+        else {
+            cout << "NONE" << endl;
+        }
+    }
+
+    return 0;
+}
