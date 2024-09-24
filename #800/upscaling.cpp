b17bb157ba1b1e75ccd6aeacf0a1e568fb75ca38
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-09-24 12:05:00 +0600

    1950B - Upscaling

---
+++ #800/upscaling.cpp
@@ -0,0 +1,35 @@
+//1950B - Upscaling
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if((j+i)%2 == 0) {
+                    cout << "##";
+                }
+                else {
+                    cout << "..";
+                }
+            }
+            cout << endl;
+            for(int j=0; j<n; j++) {
+                if((j+i)%2 == 0) {
+                    cout << "##";
+                }
+                else {
+                    cout << "..";
+                }
+            }
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
