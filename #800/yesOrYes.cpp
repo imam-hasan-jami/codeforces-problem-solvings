fe1da19995c1d056ecbe12cf49f45e6ed82aaab4
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-10-03 12:30:11 +0600

    1703A - YES or YES?

---
+++ #800/yesOrYes.cpp
@@ -0,0 +1,26 @@
+//1703A - YES or YES?
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        string s;
+        cin >> s;
+
+        for(int i=0; i<3; i++) {
+            s[i] = tolower(s[i]);
+        }
+        
+        if(s == "yes") {
+                cout << "YES" << endl;
+            }
+            else {
+                cout << "NO" << endl;
+            }
+    }
+
+    return 0;
+}
