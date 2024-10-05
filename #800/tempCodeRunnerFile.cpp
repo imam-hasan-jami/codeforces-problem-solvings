8751ff3ab9bcff099f4d2d5b74ade62e2cc08ff4
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-10-06 01:28:39 +0600

    118A - String Task (1000)
    1000 problem

---
+++ #800/tempCodeRunnerFile.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s, result = "";
+    cin >> s;
+
+    for(int i=0; i<s.size(); i++) {
+        s[i] = (char)tolower(s[i]);
+        if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'y') {
+            continue;
+        } 
+        else {
+            result += ".";
+            result += s[i];
+        }
+    }
+
+    cout << result << endl;
+
+    return 0;
+}
