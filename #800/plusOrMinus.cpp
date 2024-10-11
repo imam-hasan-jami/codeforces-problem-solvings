37e85f0d0c6fe0c7b4b98ca081b541b666c3a69d
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-10-11 16:02:36 +0600

    1807A - Plus or Minus

---
+++ #800/plusOrMinus.cpp
@@ -0,0 +1,15 @@
+//1807A - Plus or Minus
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int t,a,b,c;
+    cin >> t;
+    while(t--) {
+        cin >> a >> b >> c;
+        if(a+b==c) cout << "+" << endl;
+        else cout << "-" << endl;
+    }
+    return 0;
+}
