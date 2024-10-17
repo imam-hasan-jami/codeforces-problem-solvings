0fcae4f663f68de5aa22eadd602526826cf4c1e7
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-10-17 18:42:47 +0600

    1850A - To My Critics

---
+++ #800/toMyCritics.cpp
@@ -0,0 +1,21 @@
+//1850A - To My Critics
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int t;
+    cin >> t;
+    while(t--) {
+      int a,b,c;
+      cin >> a >> b >> c;
+      
+      if(a+b >= 10) cout << "YES" << endl;
+      else if(a+c >= 10) cout << "YES" << endl;
+      else if(b+c >= 10) cout << "YES" << endl;
+      else cout << "NO" << endl;
+    }
+    
+    return 0;
+}
