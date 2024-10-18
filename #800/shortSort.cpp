025a931bee89a7d068bacd8491fe6de5b1ccba6b
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-10-19 00:06:13 +0600

    1873A - Short Sort

---
+++ #800/shortSort.cpp
@@ -0,0 +1,17 @@
+//1873A - Short Sort
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        string s;
+        cin >> s;
+        if(s == "bca" || s == "cab") cout << "NO" << endl;
+        else cout << "YES" << endl;
+    }
+
+    return 0;
+}
